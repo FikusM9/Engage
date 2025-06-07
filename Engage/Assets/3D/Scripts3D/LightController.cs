e085f9e92f06f10a2bf6bff7b45e85726cbd6b79
vukperisic03 <vukperisic03@gmail.com>
2025-06-07 20:51:18 +0200

    lights

--- Engage/Assets/3D/Scripts3D/LightController.cs
+++ Engage/Assets/3D/Scripts3D/LightController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public bool isOn = true;
+
+    public Light pointLight;
+    float minIntensity = 0.8f;
+    float maxIntensity = 1.2f;
+    float minTime = 1f;
+    float maxTime = 2f;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isOn) return;
+        isOn = true;
+        gameObject.SetActive(true);
+        StartCoroutine(Flicker());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(!isOn) return;
+        isOn = false;
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator Flicker()
+    {
+        while (isOn)
+        {
+            pointLight.intensity = Random.Range(minIntensity, maxIntensity);
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        }
     }
 }
