28788b352ac553f3a54851d7b0dcc7caf5412703
vukperisic03 <vukperisic03@gmail.com>
2025-06-08 14:16:22 +0200

    door

--- Engage/Assets/3D/Scripts3D/PlayerController3D.cs
+++ Engage/Assets/3D/Scripts3D/PlayerController3D.cs
@@ -45,6 +45,8 @@ public class PlayerController3D : MonoBehaviour
     AudioSource audioSource;
 
     bool isWalking = false;
+
+    bool isDoorOpen = false;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -214,6 +216,12 @@ public class PlayerController3D : MonoBehaviour
         if (other.gameObject.CompareTag("CheckPoint"))
         {
             GameManager.CurrentCheckPointIndex= other.gameObject.transform.GetSiblingIndex();
+        }
+        else if (other.gameObject.CompareTag("Door") && !isDoorOpen)
+        {
+            Animator doorAnimator = other.gameObject.GetComponent<Animator>();
+            doorAnimator.SetTrigger("Open");
+            isDoorOpen = true;
         }
     }
 
